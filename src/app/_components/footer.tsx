24681521c9649501fc567fd97d5aa22b4fecb4dd
Paul Alisir <contact@paulalisir.com>
2024-07-17 13:46:27 -0400

    updated footer

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -4,11 +4,12 @@ export function Footer() {
   return (
     <footer className="border-t">
       <Container>
-        <div className="py-10 flex flex-col lg:flex-row items-center">
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a href="https://nextjs.org/docs/app/building-your-application/routing/layouts-and-templates">
-              Read Documentation
-            </a>
+        <div className="text-neutral-600 font-serif text-sm py-5 flex align-middle justify-center space-x-16">
+          <div className="flex justify-center">
+            <a href="mailto:contact@paulalisir.com">Contact</a>
+          </div>
+          <div className="flex justify-center">
+            <a href="/mentions-legales">Mentions Légales</a>
           </div>
         </div>
       </Container>
